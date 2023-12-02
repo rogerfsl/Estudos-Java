cadf97c24e603e84774eeaab6d603377c6ba5230
Roger Felipe <120397994+rogerfsl@users.noreply.github.com>
2023-12-02 16:21:04 -0300

    Exceção personalizada e programação defensiva
    criadas exceções, e métodos para saber se tem peças no tabuleiro

---
+++ src/ChessProject/Bordgame/BoardException.java
@@ -0,0 +1,13 @@
+package ChessProject.Bordgame;
+
+public class BoardException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public BoardException(String msg) {
+		super(msg);
+	}
+
+	
+
+}
