d4d0847e8feb4639f07979eda0b7c2e4c5e2ecec
Roger Felipe <120397994+rogerfsl@users.noreply.github.com>
2023-12-06 19:47:07 -0300

    Movendo peças
    métodos para mover as peças adicionados.

--- src/ChessProject/Program.java
+++ src/ChessProject/Program.java
@@ -1,15 +1,35 @@
 package ChessProject;
 
+import java.util.Scanner;
+
 import ChessProject.Bordgame.Board;
 import ChessProject.Bordgame.Position;
 import ChessProject.Chess.ChessMatch;
+import ChessProject.Chess.ChessPiece;
+import ChessProject.Chess.ChessPosition;
 
 public class Program {
 	
 	public static void main(String[] args) {
 		
+		Scanner scan = new Scanner(System.in);
+		
 		ChessMatch chessMatch = new ChessMatch();
-		UI.printBoard(chessMatch.getPieces());
+		
+		while(true) {
+			UI.printBoard(chessMatch.getPieces());
+			System.out.println();
+			System.out.print("Source: ");
+			ChessPosition source = UI.readChessPosition(scan);
+			
+			System.out.println();
+			System.out.print("Target: ");
+			ChessPosition target = UI.readChessPosition(scan);
+			
+			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			
+		}
+		
 	}
 
 }
--- src/ChessProject/UI.java
+++ src/ChessProject/UI.java
@@ -1,6 +1,10 @@
 package ChessProject;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 import ChessProject.Chess.ChessPiece;
+import ChessProject.Chess.ChessPosition;
 import ChessProject.Chess.Color;
 
 public class UI {
@@ -25,6 +29,17 @@ public class UI {
 	public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+	
+	public static ChessPosition readChessPosition(Scanner scan) {
+		try {
+			String s = scan.next();
+			char column = s.charAt(0);
+			int row = Integer.parseInt(s.substring(1));
+			return new ChessPosition(column, row);
+		} catch (RuntimeException e) {
+			throw new InputMismatchException("Error readin ChessPosition. Valid values are from a1 to h8.");
+		}
+	}
 
 
 	public static void printBoard(ChessPiece[][] pieces) {
