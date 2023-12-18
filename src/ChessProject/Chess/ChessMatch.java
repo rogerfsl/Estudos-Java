a0d127313db5704b8ca4172c04c17c2b9d5ebcf0
Roger Felipe <120397994+rogerfsl@users.noreply.github.com>
2023-12-18 18:02:49 -0300

    Ajustes na jogada especial promoção

--- src/ChessProject/Chess/ChessMatch.java
+++ src/ChessProject/Chess/ChessMatch.java
@@ -1,6 +1,6 @@
 package ChessProject.Chess;
 
-import java.security.InvalidParameterException;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -124,7 +124,7 @@ public class ChessMatch {
 			throw new IllegalStateException("There is no piece to be promoted.");
 		}
 		if(!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
-			throw new InvalidParameterException("Invalid Type for promotion.");
+			return promoted;
 		}
 		Position pos = promoted.getChessPosition().toPosition();
 		Piece p = board.removePiece(pos);
--- src/ChessProject/Program.java
+++ src/ChessProject/Program.java
@@ -45,6 +45,10 @@ public class Program {
 				if(chessMatch.getPromoted() != null) {
 					System.out.print("Enter piece for promotion (B/N/R/Q): ");
 					String type = scan.next().toUpperCase();
+					while(!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
+						System.out.print("Invalid value! Enter piece for promotion (B/N/R/Q): ");
+						type = scan.next().toUpperCase();
+					}
 					chessMatch.replacePromotedPiece(type);
 				}
 				
